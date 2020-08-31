9a4b28e2233d2d28caf67b0f2df82fc98df10308
YPAzevedo <yago.pereira.az@gmail.com>
2020-08-31 07:44:21 -0300

    💬📱 mode start server code to separate module

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-const { ApolloServer, PubSub } = require("apollo-server");
-const jwt = require("jsonwebtoken");
+const startServer = require("./server");
 
-const typeDefs = require("./typeDefs");
-const resolvers = require('./resolvers')
-
-const pubsub = new PubSub();
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    const token =
-      req && req.headers.authorization ? req.headers.authorization : "";
-    const user = token ? jwt.verify(token, "secret") : null;
-
-    return {
-      user,
-      pubsub,
-    };
-  },
-});
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+startServer();
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+function startServer() {
+  const pubsub = new PubSub();
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      const token =
+        req && req.headers.authorization ? req.headers.authorization : "";
+      const user = token ? jwt.verify(token, "secret") : null;
+
+      return {
+        user,
+        pubsub,
+      };
+    },
+  });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+}
+
+module.exports = startServer;
